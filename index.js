d6be9230461893421ba14b9269869f8316b2035d
benedict <kiokobenedict@gmail.com>
2021-02-16 16:35:15 +0300

    added posts and users schemas

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
-const { Keystone } = require('@keystonejs/keystone');
-const { GraphQLApp } = require('@keystonejs/app-graphql');
-const { AdminUIApp } = require('@keystonejs/app-admin-ui');
-const { MongooseAdapter: Adapter } = require('@keystonejs/adapter-mongoose');
-const PROJECT_NAME = 'blog';
-const adapterConfig = { mongoUri: 'mongodb://localhost/blog' };
+const dotenv = require("dotenv").config();
+const { Keystone } = require("@keystonejs/keystone");
+const { GraphQLApp } = require("@keystonejs/app-graphql");
+const { AdminUIApp } = require("@keystonejs/app-admin-ui");
 
+const { MongooseAdapter: Adapter } = require("@keystonejs/adapter-mongoose");
 
-/**
- * You've got a new KeystoneJS Project! Things you might want to do next:
- * - Add adapter config options (See: https://keystonejs.com/keystonejs/adapter-mongoose/)
- * - Select configure access control and authentication (See: https://keystonejs.com/api/access-control)
- */
+const PROJECT_NAME = "blog";
+const adapterConfig = {
+  mongoUri: process.env.MONGO_URI,
+};
+
+const PostSchema = require("./lists/Post");
+const UserSchema = require("./lists/User");
 
 const keystone = new Keystone({
   adapter: new Adapter(adapterConfig),
+  cookieSecret: process.env.COOKIE_SECRET,
 });
+
+keystone.createList("Post", PostSchema);
+keystone.createList("User", UserSchema);
 
 module.exports = {
   keystone,
-  apps: [new GraphQLApp(), new AdminUIApp({ name: PROJECT_NAME, enableDefaultRoute: true })],
+  apps: [
+    new GraphQLApp(),
+    new AdminUIApp({
+      name: PROJECT_NAME,
+      enableDefaultRoute: true,
+    }),
+  ],
 };
